c9d4e333daf3b876b52e83be8fe5422a4f4e5800
youngjinss <youngjins@kakao.com>
2025-03-17 14:00:59 +0900

    수정

---
+++ shell/install.sh
@@ -0,0 +1,6 @@
+cd ./lib/abides-jpmc-public/abides-core
+python setup.py install
+cd ./lib/abides-jpmc-public/abides-markets
+python setup.py install
+cd ./lib/abides-jpmc-public/abides-gym
+python setup.py install

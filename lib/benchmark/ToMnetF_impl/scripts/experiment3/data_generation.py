3165232d92c3186466725dc5a84a77c0621d98ca
youngjinss <youngjins@kakao.com>
2025-07-05 00:30:58 +0900

    수정

--- lib/benchmark/ToMnetF_impl/scripts/experiment3/data_generation.py
+++ lib/benchmark/ToMnetF_impl/scripts/experiment3/data_generation.py
@@ -117,11 +117,8 @@ class DataReader:
                 current_timestep = int(
                     x.strip().replace("Timestep_", "").replace(":", "")
                 )
-<<<<<<< HEAD
                 sr_maps[gamma_str] = sr_values.reshape(
-                    self.MAZE_WIDTH, self.MAZE_HEIGHT
-                )
-=======
+                    self.MAZE_WIDTH, self.MAZE_HEIGHT                
                 if current_timestep not in sr_data_per_timestep:
                     sr_data_per_timestep[current_timestep] = {}
                 continue
